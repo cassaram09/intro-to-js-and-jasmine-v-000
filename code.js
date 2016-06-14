d26824c3a288992c1b6bce1b5243d28fb17fea57
cassaram09 <cassaram09@gmail.com>
2016-06-14 00:26:36 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(iceCream) {
+  return `I love ${iceCream}`
+}
+
+function shouting(string) {
+  return string.toUpperCase()
+}
+
+function roundDown(number) {
+  return Math.floor(number)
+}
+
+function theTruth() {
+  return true
+}
